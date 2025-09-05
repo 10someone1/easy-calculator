f5f8398a343b4988772b8f405d1ad9b372420e13
10someone1 <ziqaes.01@gmail.com>
2025-09-05 09:39:52 +0700

    Add files via upload

---
+++ easy_calculator.py
@@ -0,0 +1,36 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "\033[31mERROR\033[0m"
+    return a / b
+def power(a, b):
+    return a ** b
+
+print("____eassy calculator____")
+print("1. add")
+print("2. subtract")
+print("3. multiply")
+print("4. divide")
+print("5. power")
+a = float(input("Enter number 1: "))
+b = float(input("Enter number 2: "))
+operation = int(input("Enter operation choice: "))
+print("")
+
+if operation == 1 or "add":
+    print(f"{a} + {b} = {add(a, b)}")
+elif operation == 2 or "subtract":
+    print(f"{a} - {b} = {subtract(a, b)}")
+elif operation == 3 or "multiply":
+    print(f"{a} * {b} = {multiply(a, b)}")
+elif operation == 4 or "divide":
+    print(f"{a} / {b} = {divide(a, b)}")
+elif operation == 5 or "power":
+    print(f"{a} ** {b} = {power(a, b)}")
+else:
+    print("invalid choice")
